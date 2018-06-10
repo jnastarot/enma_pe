f19061e110a82cf2fcfd0c09d19c4d01d55deab1
JNA <jnastarot@yandex.ru>
2018-06-10 10:31:46 +0300

    gcc fix

--- enma_pe/enma_pe_internal.h
+++ enma_pe/enma_pe_internal.h
@@ -1,9 +1,6 @@
 #pragma once
 
-//#pragma warning(push)  
 #pragma pack(push,1)
-
-//#pragma warning(disable:4005)
 
 //
 // Image Format
@@ -1009,4 +1006,45 @@ typedef struct image_cor20_header
 } image_cor20_header, *pimage_cor20_header;
 
 #pragma pack(pop)
-//#pragma warning(pop) 
+
+#ifndef ALIGN_UP
+    #define ALIGN_UP(a, b) ((((a) + (b) - 1) / (b)) * (b))
+#endif
+
+#ifndef offsetof
+    #define offsetof(s,m) (uint32_t)((size_t)&(((s*)0)->m))
+#endif
+
+#ifndef max
+    #define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+    #define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+#define INT8_SIZE   1
+#define INT16_SIZE  2
+#define INT32_SIZE  4
+#define INT64_SIZE  8
+#define CHAR_SIZE   1
+#define WCHAR_SIZE  2
+
+#ifndef _In_
+    #define _In_
+#endif
+#ifndef _Inout_
+    #define _Inout_
+#endif
+#ifndef _Out_opt_
+    #define _Out_opt_
+#endif
+#ifndef _In_opt_
+    #define _In_opt_
+#endif
+#ifndef _Inout_opt_
+    #define _Inout_opt_
+#endif
+#ifndef _Out_
+    #define _Out_
+#endif
